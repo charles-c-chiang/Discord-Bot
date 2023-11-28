850ce2f542d552497c18438982fefff8475b6e26
charles-c-chiang <cchiang3@nd.edu>
2023-11-28 17:34:50 -0500

    working gpt integration

--- example_bot.py
+++ example_bot.py
@@ -18,6 +18,12 @@ intents.message_content = True
 
 client = discord.Client(intents=intents)
 
+bot = commands.Bot(command_prefix='/', intents=intents)
+
+@bot.command()
+async def test(ctx, arg):
+    await ctx.send(arg)
+
 AIclient = AsyncAzureOpenAI(api_key = OPENAI_API_KEY,
 api_version = "2023-07-01-preview", 
 azure_endpoint="https://charlietest.openai.azure.com/openai/deployments/charlieTestModel/chat/completions?api-version=2023-07-01-preview")
@@ -33,24 +39,39 @@ last_message = 0
 
 @client.event
 async def on_message(message):
+    global message_text
+    global last_message
+
     if message.author == client.user:
         return
 
-    global last_message
+    if message.content.startswith('$hello'):
+        await message.channel.send('Hello!')
+        return
+    
+    # Dump conversation to file and quit
+    if message.content.startswith('$quit'):
+        await message.channel.send('Goodbye!')
+        with open("convo.json", "w") as f:
+            json.dump(message_text, f, indent=4)
+        await client.close()
+        return
 
-    """AI STUFF HERE"""
+    # create log of conversation for client
+    message_text.append({"role": "user", "content": message.content,})
+
+    # send conversation to aclient, await response
     completion = await AIclient.chat.completions.create(
         model="deployment-name",  # e.g. gpt-35-instant
-        messages=[
-            {"role": "user", "content": message.content,},
-        ],
+        messages=message_text
     )
+    # add to log of conversation
+    message_text.append({"role": "assistant", "content" : completion.choices[0].message.content})
+    print(message_text)
+
+    # send aclient message to discord
     print(completion.choices[0].message.content)
     await message.channel.send(completion.choices[0].message.content)
-    """END AI STUFF"""
-
-    # if message.content.startswith('$hello'):
-    #     await message.channel.send('Hello!')
 
     # elif message.content.startswith('$typing'):
     #     channel = client.get_channel(1169885828265279508)
@@ -58,14 +79,14 @@ async def on_message(message):
     #     await asyncio.sleep(3)
     #     await channel.send('Done typing!')
 
+    # message_to_send = 'You said: "' + message.content + '"'
+    # await message.channel.send(message_to_send)
+
+    # if last_message == 0:
+    #     await message.channel.send('this is the first message!')
     # else :
-    #     message_to_send = 'You said: "' + message.content + '"'
-    #     await message.channel.send(message_to_send)
-    #     if last_message == 0:
-    #         await message.channel.send('this is the first message!')
-    #     else :
-    #         await message.channel.send(f'time since last message: {time.time() - last_message} seconds')
-    #     last_message = time.time()
+    #     await message.channel.send(f'time since last message: {time.time() - last_message} seconds')
+    # last_message = time.time()
 
 # @client.event
 # async def on_typing(channel, user, when):
